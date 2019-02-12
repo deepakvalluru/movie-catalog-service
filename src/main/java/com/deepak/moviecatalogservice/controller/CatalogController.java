af227d099009c27213761b84b797d650716aae70
Deepu <Deepu@DESKTOP-VA5DMJC>
2019-02-11 20:31:44 -0600

    Call other micro services

--- src/main/java/com/deepak/moviecatalogservice/controller/CatalogController.java
+++ src/main/java/com/deepak/moviecatalogservice/controller/CatalogController.java
@@ -4,24 +4,52 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.deepak.moviecatalogservice.model.CatalogItem;
+import com.deepak.moviecatalogservice.model.Movie;
+import com.deepak.moviecatalogservice.model.Rating;
 
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping ( "/catalog" )
 public class CatalogController
 {
-   @RequestMapping(method=RequestMethod.GET, path = "/{userId}")
-   public List<CatalogItem> getUserCatalog( @PathVariable("userId") String userId )
+
+   @Autowired
+   private RestTemplate restTemplate;
+
+   @RequestMapping ( method = RequestMethod.GET, path = "/{userId}" )
+   public List< CatalogItem > getUserCatalog( @PathVariable ( "userId" ) String userId )
    {
-      return getCatalogItems();
+      ResponseEntity< List< Rating > > ratingList =
+                                                  this.restTemplate.exchange( "http://localhost:8083/ratings/users/"
+                                                                              + userId,
+                                                                              HttpMethod.GET,
+                                                                              null,
+                                                                              new ParameterizedTypeReference< List< Rating > >()
+                                                                              {
+                                                                              } );
+      
+      
+      return ratingList.getBody().stream().map( rating -> {
+         Movie movie = this.restTemplate.getForEntity( "http://localhost:8082/movies/" + rating.getMovieId(),
+                                                       Movie.class )
+                                        .getBody();
+         return new CatalogItem( movie.getName(), movie.getDesc(), rating.getRating() );
+      }
+      ).collect( Collectors.toList() );
+      
    }
-   
-   private List<CatalogItem> getCatalogItems()
+
+   private List< CatalogItem > getCatalogItems()
    {
       return Stream.< CatalogItem > builder()
                    .add( new CatalogItem( "Hello", "Hello-Desc", 5 ) )
